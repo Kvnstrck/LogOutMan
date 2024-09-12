370bf87d52e0bf7870a5e181c38c914abd6b54ce
Kvnstrck <strackekevin@gmail.com>
2024-09-12 19:07:47 +0200

    WIP: wip on install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+while getopts "hva:" flag; do
+ case $flag in
+   h) # Handle the -h flag
+   # Display script help information
+   cat ./README.md
+   exit 1
+
+   ;;
+   v) # Handle the -v flag
+   # Enable verbose mode
+   ;;
+   a)
+     # append the window rules to the hyprland config
+    echo "windowrule=float,^(LogOutMan)$
+           windowrule=move 35 770,title:^(LogOutMan)(.*)$" >> ~/.config/hypr/hyprland.conf
+   ;;
+   \?)
+   # Handle invalid options
+   echo "unknown flag, aborting!"
+   exit 1
+   ;;
+ esac
+done
+
+# build the project
+mkdir -p build
+# shellcheck disable=SC2164
+cd build
+cmake ..
+make -j
+cd ./..
+
+# move config files
+mkdir -p ~/.config/LogOutMan/
+cp ./LogOutMan.json ~/.config/LogOutMan/LogOutMan.json
+cp ./main.css ~/.config/LogOutMan/main.css
+
+
